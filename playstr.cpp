c9f2be38a2e04328884508d44b4121165d7ad703
Arjun <arj.kashyap123@gmail.com>
2019-07-26 23:45:47 +0530

    codechef.com/playstr

--- playstr.cpp
+++ playstr.cpp
@@ -2,55 +2,22 @@
 #include<string>
 using namespace std;
 
-bool isPresent(int n, string st1, string st2){
-    int index = n;
-    bool present = false;
-    while(index < st1.length()){
-        if(st1[n] == st2[index]){ 
-            present = true;
-            break; }
-        
-        index++;
-    }
-    return present;
-}
-
 int main(){
-    int t, n;
-    string s1, s2;
+    int t;
     cin>>t;
     while(t--){
-        cin>>n;
-        if(n != 0){
-        cin>>s1;
-        cin>>s2;
-        for(int i = 0; i < n; i++){
-            int j;
-
-            if(s1[i] == s2[i]){ continue;}
-            else{
-                if(isPresent(i, s1, s2)){
-                    int j = i;
-                    while(j <= n){
-                        if(s1[i] == s2[j]){
-                            char temp = s2[i];
-                            s2[i] = s2[j];
-                            s2[j] = temp;
-                 
-                        }
-                        j++;
-                    }
-                        
-                }
-                else{
-                    cout<<"NO"<<endl;
-                    break;
-                }
-            }
-            if (s1 == s2){ cout<<"YES"<<endl; }
+        int n, c1 = 0, c2 = 0;
+        string s1, s2;
+        cin>>n>>s1>>s2;
+        for(int i = 0; i < n; i++ ){
+            if(s1.at(i) == '1'){ c1 += 1; }
+            if(s2.at(i) == '1'){ c2 += 1; }
         }
-      }
-      else{ continue; }
-   }
+        if( c1 == c2 )
+            cout<<"YES"<<endl;
+        else
+            cout<<"NO"<<endl;
+
+    }
     return 0;
 }
