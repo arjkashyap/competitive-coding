9cccc289aa02901922894fc9325a1d86b3fb49b6
Arjun <arj.kashyap123@gmail.com>
2020-02-03 13:26:35 +0530

    fix sparse Arrays

--- hackerRank/challenges/sparseArrays.cpp
+++ hackerRank/challenges/sparseArrays.cpp
@@ -4,36 +4,33 @@ using namespace std;
 
 int main()
 {
-    ios::sync_with_stdio(false);
-    int n, q;
-    cin >> n;
+    int s, q;
+    cin >> s;
     vector<string> str;
-    vector<string> quer;
-    vector<int> res;
-    for(int i = 0; i < n; i++){
+
+    for(int i = 0; i < s; i++)
+    {
         string s;
         cin >> s;
         str.push_back(s);
     }
-
+    
     cin >> q;
-    for(int i = 0; i < q; i++){
-        string q;
-        cin >> q;
-        quer.push_back(q);
-    }
-
-    for(int i = 0; i < q; i++){
+    vector<string> quer;
+    vector<int> res;
+    for(int i = 0; i < q; i++)
+    {
         int count = 0;
-        for(int j = 0; j < n; j++){
-            if(quer[i] == str[j])
-                count ++;
+        string qstr;
+        cin >> qstr;
+        for(int j = 0; j < s; j++){
+            if(qstr == str[j])
+                count++;
         }
         res.push_back(count);
     }
-
-    for(auto i : res)
-        cout << res[i] << endl;
+   for(int i = 0; i < res.size(); i++)
+      cout << res[i] << "\n";
 
     return 0;
 }
