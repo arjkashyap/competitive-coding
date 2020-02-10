04d020e93c4039f4c9637dc15465c201fe70b220
Arjun <arj.kashyap123@gmail.com>
2020-02-11 02:18:22 +0530

    Insert node at a specific position in a linked list

---
+++ hackerRank/challenges/insertNodeAtSpecPos.cpp
@@ -0,0 +1,91 @@
+// Insert node at a specific position in a linked list
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class SinglyLinkedListNode {
+    public:
+        int data;
+        SinglyLinkedListNode *next;
+
+        SinglyLinkedListNode(int node_data) {
+            this->data = node_data;
+            this->next = nullptr;
+        }
+};
+
+class SinglyLinkedList {
+    public:
+        SinglyLinkedListNode *head;
+        SinglyLinkedListNode *tail;
+
+        SinglyLinkedList() {
+            this->head = nullptr;
+            this->tail = nullptr;
+        }
+
+        void insert_node(int node_data) {
+            SinglyLinkedListNode* node = new SinglyLinkedListNode(node_data);
+
+            if (!this->head) {
+                this->head = node;
+            } else {
+                this->tail->next = node;
+            }
+
+            this->tail = node;
+        }
+};
+
+void print_singly_linked_list(SinglyLinkedListNode* node, string sep, ofstream& fout) {
+    while (node) {
+        fout << node->data;
+
+        node = node->next;
+
+        if (node) {
+            fout << sep;
+        }
+    }
+}
+
+void free_singly_linked_list(SinglyLinkedListNode* node) {
+    while (node) {
+        SinglyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+// Complete the insertNodeAtPosition function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+SinglyLinkedListNode* insertNodeAtPosition(SinglyLinkedListNode* head, int data, int position) {
+    SinglyLinkedListNode* ptr = new SinglyLinkedListNode(data);
+    int count = 1;
+    if(head == nullptr || position == 0){
+        head = ptr;
+    }
+    else{
+        SinglyLinkedListNode* tmp = head;
+        while(count != position){
+            tmp = tmp -> next;
+            count++;
+        }
+        SinglyLinkedListNode* temploc = tmp-> next;
+        tmp -> next = ptr;
+        ptr -> next = temploc;
+    }
+    return head;
+}
+
+int main()
