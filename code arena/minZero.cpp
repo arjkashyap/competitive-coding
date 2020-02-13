f812fac7a12c983591ad4d843cb667069abd53e3
Arjun <arj.kashyap123@gmail.com>
2020-02-13 22:08:10 +0530

    erasing zeros

---
+++ code arena/minZero.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+#include<string>
+#include<math.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    int t;
+    cin >> t;
+    while(t--){
+        unordered_map<int, int> map;
+        int count = 0;
+        string arr;
+        cin >> arr;
+        int size = arr.length(); 
+        for(int i = 0; i < size; i++){
+            if( arr[i] == '1' ){
+                // 1 not present in map
+                if(map.find(arr[i]) == map.end()){
+                    map[arr[i]] = i;
+                }
+                // 1 is present
+                else{
+                    int index = -1;
+                    index = map['1'];
+                    count += i - index - 1;
+                    map.erase('1');
+                }
+            }
+        }
+        cout << count << "\n";
+    }
+    return 0;
+}
