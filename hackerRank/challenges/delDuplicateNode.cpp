012bdcf07be8d45c37312206b8ad97b1db3fde15
Arjun <arj.kashyap123@gmail.com>
2020-02-26 04:17:08 +0530

    {}

---
+++ hackerRank/challenges/delDuplicateNode.cpp
@@ -0,0 +1,93 @@
+// Delete duplicate nodes from a linked list
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class SinglyLinkedListNode {
+    public:
+        int data;
+        SinglyLinkedListNode *next;
+
+        SinglyLinkedListNode(int node_data) {
+            this->data = node_data;
+            this->next = nullptr;
+        }
+};
+
+class SinglyLinkedList {
+    public:
+        SinglyLinkedListNode *head;
+        SinglyLinkedListNode *tail;
+
+        SinglyLinkedList() {
+            this->head = nullptr;
+            this->tail = nullptr;
+        }
+
+        void insert_node(int node_data) {
+            SinglyLinkedListNode* node = new SinglyLinkedListNode(node_data);
+
+            if (!this->head) {
+                this->head = node;
+            } else {
+                this->tail->next = node;
+            }
+
+            this->tail = node;
+        }
+};
+
+void print_singly_linked_list(SinglyLinkedListNode* node, string sep, ofstream& fout) {
+    while (node) {
+        fout << node->data;
+
+        node = node->next;
+
+        if (node) {
+            fout << sep;
+        }
+    }
+}
+
+void free_singly_linked_list(SinglyLinkedListNode* node) {
+    while (node) {
+        SinglyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+// Complete the removeDuplicates function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+
+SinglyLinkedListNode* deleteNode(SinglyLinkedListNode* head)
+{
+    SinglyLinkedListNode* tmp = head;
+    head = head -> next;
+    delete tmp;
+    return head;
+}
+
+SinglyLinkedListNode* removeDuplicates(SinglyLinkedListNode* head) {
+    SinglyLinkedListNode* ptr = head;
+    while(ptr -> next){
+        if((ptr -> data) == ((ptr -> next) -> data))
+            ptr -> next = deleteNode(ptr -> next);
+        else
+            ptr = ptr -> next;
+    }
+    return head;
+}
+
+int main()
